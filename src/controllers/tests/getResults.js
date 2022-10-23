72b3ac5c59de4aee526637ff78b42efaa262145f
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-10-23 21:22:04 +0300

    front test

--- src/controllers/tests/getResults.js
+++ src/controllers/tests/getResults.js
@@ -1,33 +1,16 @@
 const { Test } = require("../../models/test");
 
 const getResults = async (req, res) => {
-  const { value } = req.body;
+  const { value: userAnswers } = req.body;
 
-  const rightAnswersCounter = await value.reduce(async (acc, el) => {
+  let rightAnswersCounter = 0;
+  for (let el of userAnswers) {
     const dbQuestion = await Test.findOne({ _id: el._id });
 
     if (dbQuestion && dbQuestion.rightAnswer === el.answer) {
-      acc += 1;
+      rightAnswersCounter += 1;
     }
-
-    return acc;
-  }, 0);
-
-  // const questions = await Test.find();
-  // let acc = 0;
-  // for (const question of questions) {
-  //   userAnswers.forEach((userAnswer) => {
-  //     console.log(`${question._id}` === `new ObjectId("${userAnswer._id}")`);
-  //     if (question._id === `new ObjectId("${userAnswer._id}")`) {
-  //       if (question.rightAnswer === userAnswer.answer) {
-  //         acc += 1;
-  //       }
-  //     }
-  //   });
-    // if (acc === 12) {
-    //   break;
-    // }
-  // }
+  }
 
   res.json({
     rightAnswers: rightAnswersCounter,
@@ -35,17 +18,3 @@ const getResults = async (req, res) => {
 };
 
 module.exports = getResults;
-
-// {
-//   _id: new ObjectId("634e71e6b739faa5926ec29b"),
-//   type: 'theory',
-//   question: 'navigate().back(). This navigation command in Selenium:',
-//   answers: [
-//     'Returns the user to the previous page from the browser history',
-//     'Allows the user to navigate to the next page according to the browser history',
-//     'Used to refresh the browser page',
-//     'Allows the user to open a new window in the browser and follow the specified URL',
-//     'There is no such command'
-//   ],
-//   rightAnswer: 'Returns the user to the previous page from the browser history'
-// }
