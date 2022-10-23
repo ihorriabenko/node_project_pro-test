4fbcff702d037c439be05d91c699e673100acab8
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-10-23 20:09:13 +0300

    front test

--- src/controllers/tests/getResults.js
+++ src/controllers/tests/getResults.js
@@ -1,21 +1,37 @@
 const { Test } = require("../../models/test");
 
 const getResults = async (req, res) => {
-  const {value} = req.body;
+  const { value } = req.body;
 
-  const rightAnswersCounter = await value.reduce(async (acc, el) => {
-    const dbQuestion = await Test.findOne({ _id: el._id });
+  // const rightAnswersCounter = await value.reduce(async (acc, el) => {
+  //   const dbQuestion = await Test.findOne({ _id: el._id });
 
-    if (dbQuestion && dbQuestion.rightAnswer === el.answer) {
-      acc += 1;
-    }
+  //   if (dbQuestion && dbQuestion.rightAnswer === el.answer) {
+  //     acc += 1;
+  //   }
 
-    return acc;
-  }, 0);
+  //   return acc;
+  // }, 0);
+
+  const questions = await Test.find();
 
   res.json({
-    rightAnswers: rightAnswersCounter,
+    rightAnswers: 7,
   });
 };
 
 module.exports = getResults;
+
+// {
+//   _id: new ObjectId("634e71e6b739faa5926ec29b"),
+//   type: 'theory',
+//   question: 'navigate().back(). This navigation command in Selenium:',
+//   answers: [
+//     'Returns the user to the previous page from the browser history',
+//     'Allows the user to navigate to the next page according to the browser history',
+//     'Used to refresh the browser page',
+//     'Allows the user to open a new window in the browser and follow the specified URL',
+//     'There is no such command'
+//   ],
+//   rightAnswer: 'Returns the user to the previous page from the browser history'
+// }
