3b86035be0658f313a413e171c55050c799ef4fb
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-10-24 13:17:22 +0300

    cors

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const cors = require("cors");
 const routes = require("./src/routes/api");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerDocument = require('./swagger.json');
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
