df3079a9d0be5c53dcd182449f644ea6853f1258
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-10-27 09:51:36 +0300

    -modules

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,12 +2,10 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
 const authenticateSocial = require("./authenticate-social");
-const uploadMiddleware = require("./upload");
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
   authenticateSocial,
-  uploadMiddleware,
 };
